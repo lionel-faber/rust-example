3c0657861030266735b920fff8ddbc9dcd2487b4
Lionel Faber <lionel1704@gmail.com>
2019-09-03 16:32:05 +0530

    go for gold

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+pub mod calculator {
+    pub fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    pub fn sub(a: i32, b: i32) -> i32 {
+        a - b
+    }
+}
+
+mod reproduce_issue;
+
+#[cfg(test)]
+mod tests {
+    use super::calculator::*;
+
+    #[test]
+    fn add_correctly_adds_2_integers() {
+        assert_eq!(add(2, 2), 4);
+    }
+
+    #[test]
+    fn sub_correctly_subtracts_2_integers() {
+        assert_eq!(sub(6, 2), 4);
+    }
+}
---
+++ src/reproduce_issue.rs
@@ -0,0 +1,6 @@
+use crate::calculator::add;
+
+#[test]
+pub fn test() {
+    assert_eq!(add(1, 2), 4);
+}
