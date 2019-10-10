6cd67bed6c3692961e706642f4bf0419c369e483
Lionel Faber <lionel1704@gmail.com>
2019-10-10 22:53:29 +0530

    testing sed again

--- actions/issue-doctor/entrypoint.sh
+++ actions/issue-doctor/entrypoint.sh
@@ -9,17 +9,13 @@ fi
 API_HEADER="Accept: application/vnd.github.v3+json; application/vnd.github.antiope-preview+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
-echo $GITHUB_EVENT_PATH
-
 issue_body=$(jq --raw-output .issue.body "$GITHUB_EVENT_PATH")
 issue_number=$(jq --raw-output .issue.number "$GITHUB_EVENT_PATH")
 
 labels=''
 echo $issue_body > issue_body
-cat issue_body
-ls
-pwd
 sed -n '/```test_start/,/```/p' issue_body | sed '/^```/ d' > src/reproduce_issue.rs
+cat src/reproduce_issue.rs
 if cargo check --tests; then
     if cargo test --release reproduce_issue; then
     message='The issue is not reproducible'
