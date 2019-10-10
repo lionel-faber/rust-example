69d51391491e6b0455f8229ae31fa565ece7eb74
Lionel Faber <lionel1704@gmail.com>
2019-10-10 17:40:15 +0530

    simplify docker approach

--- actions/issue-doctor/entrypoint.sh
+++ actions/issue-doctor/entrypoint.sh
@@ -1,7 +1,21 @@
-#!/bin/sh -l
+#!/bin/bash
+set -eu
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+API_HEADER="Accept: application/vnd.github.v3+json; application/vnd.github.antiope-preview+json"
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
+echo $GITHUB_EVENT_PATH
+
+issue_body=$(jq --raw-output .issue.body "$GITHUB_EVENT_PATH")
+issue_number=$(jq --raw-output .issue.number "$GITHUB_EVENT_PATH")
 
 labels=''
-echo $1 > issue_body
+echo $issue_body > issue_body
 sed -n '/```test_start/,/```/p' issue_body | sed '/^```/ d' > src/reproduce_issue.rs
 if cargo check --tests; then
     if cargo test --release reproduce_issue; then
@@ -16,8 +30,8 @@ else
 fi
 curl -sSL \
     -H "Content-Type: application/json" \
-    -H "Authorization: token $3" \
-    -H "Accept: application/vnd.github.v3+json; application/vnd.github.antiope-preview+json" \
+    -H "${AUTH_HEADER}" \
+    -H "${API_HEADER}" \
     -X PUT \
     -d "{ \"labels\" : [$labels]}" \
-    https://api.github.com/repos/lionel1704/gh-actions-demo/issues/$2/labels
+    https://api.github.com/repos/lionel1704/gh-actions-demo/issues/${issue_number}/labels
