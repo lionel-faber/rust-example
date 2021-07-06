97b3e6006f16d87cfed11df87cfaeae988c72ad2
Lionel Faber <lionel1704@gmail.com>
2021-07-06 13:07:19 +0530

    parallel tasks test

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,19 @@
-pub mod calculator {
-    pub fn add(a: i32, b: i32) -> i32 {
-        a + b
-    }
+use tokio::time::{sleep, Duration};
 
-    pub fn sub(a: i32, b: i32) -> i32 {
-        a - b
-    }
+
+pub async fn long_task(i: usize) {
+    println!("Start - {}", i);
+    sleep(Duration::from_secs(5)).await;
+    println!("End - {}", i);
 }
 
-#[cfg(test)]
-mod reproduce_issue;
+#[tokio::test]
+async fn parallel_tasks() {
+    use futures::future::join_all;
 
-#[cfg(test)]
-mod tests {
-    use super::calculator::*;
-
-    #[test]
-    fn add_correctly_adds_2_integers() {
-        assert_eq!(add(2, 2), 4);
+    let mut futures = vec![];
+    for i in 0..5 {
+        futures.push(long_task(i));
     }
-
-    #[test]
-    fn sub_correctly_subtracts_2_integers() {
-        assert_eq!(sub(6, 2), 4);
-    }
+    let _x = join_all(futures.into_iter()).await;
 }
--- test-issue
+++
@@ -1,6 +0,0 @@
-use crate::calculator::add;
-
-#[test]
-pub fn test() {
-    assert_eq!(add(1, 2), 4);
-}
